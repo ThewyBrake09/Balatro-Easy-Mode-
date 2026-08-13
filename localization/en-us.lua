1c350aa31d53e9cad26e100cbdf4519527b9cd8f
ThewyBrake09 <8h22hadi@gmail.com>
2026-08-14 02:05:07 +0700

    Add files via upload

---
+++ localization/en-us.lua
@@ -0,0 +1,36 @@
+return {
+    descriptions = {   
+    --joker
+        Joker = {
+            j_baseball = {
+                name = "Baseball Card",
+                text = {
+"{C:green}Common{} Jokers",
+                    "each give {X:mult,C:white} X#1# {} Mult"
+                },
+            },
+        },
+    -- spectral
+        Spectral = {
+            c_ankh = {
+                name = "Ankh",
+                text = {
+                    "Create a copy of a",
+                    "random {C:attention}Joker{}",
+                },
+            },
+        
+            c_ectoplasm={
+                name="Ectoplasm",
+                text={
+                    "Add {C:dark_edition}Negative{} to",
+                    "a random {C:attention}Joker,",
+                    "{C:red}+#1#{} hand size",
+                },
+            },   
+        },
+    }    
+}
+
+
+
---
+++ localization/id.lua
@@ -0,0 +1,38 @@
+return {
+    descriptions = {
+        -- joker
+        Joker = {
+            j_baseball = {
+                name = "Kartu Bisbol",
+                text = {
+                    "Setiap {C:green}Common{} Joker",
+                    "memberikan {X:mult,C:white} X#1# {} Mult"
+                },
+            },
+        },
+    misc = {
+        labels = {
+            j_baseball = "Kartu Bisbol"
+        },
+    },
+        -- spectral
+        Spectral = {
+            c_ankh = {
+                name = "Ankh",
+                text = {
+                    "Buat sebuah salinan",
+                    "acak {C:attention}Joker{}"
+                },
+            },
+            
+            c_ectoplasm={
+                name="Ectoplasm",
+                text={
+                    "Menambah {C:dark_edition}Negatif{} ke",
+                    "{C:attention}Joker secara acak,",
+                    "{C:red}+1{} ukuran poker hand",
+                },
+            },
+        },
+    },
+}
