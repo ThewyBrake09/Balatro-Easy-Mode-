86f7fb6f7198b22a78e741cf2576a79e61a35694
ThewyBrake09 <8h22hadi@gmail.com>
2026-09-05 17:20:33 +0700

    Add files via upload

---
+++ localization/id.lua
@@ -0,0 +1,22 @@
+return = {
+    descriptions = {
+        Spectral = {
+            c_ankh = {
+                name = "Ankh",
+                text = {
+                "Buat sebuah salinan",
+                "acak {C:attention}Joker{}",
+                },
+            },
+                
+            c_ectoplasm = {
+                name = "Ectoplasma",
+                text = {
+                "Menambah {C:dark_edition}Negative{} ke",
+                "{C:attention}Joker{} secara acak,",
+                "{C:red}+1{}Poker hand",
+                },
+            },
+        },
+    },
+}
